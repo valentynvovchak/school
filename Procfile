0810fe48fc60e896a791d7015a0c6a572fb9a641
Cotyk <valentyn.vovchak@gmail.com>
2022-01-20 22:41:15 +0200

    Second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn museum_proj.wsgi --log-file -
--- museum_proj/settings.py
+++ museum_proj/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,7 +89,10 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT_MUSEUM'),
     }
 }
-
+# Update DB
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
